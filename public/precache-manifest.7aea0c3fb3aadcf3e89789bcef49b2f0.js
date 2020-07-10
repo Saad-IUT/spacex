29159fa48d94deb89ed1ee311b3e057cca86fb61
Saad <saadbinjohir@gmail.com>
2020-07-10 21:12:19 +0600

    Deploy

---
+++ public/precache-manifest.7aea0c3fb3aadcf3e89789bcef49b2f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c28ff7ea29a79d3104959c419aaa0fb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d8eefad631e36adce77",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6cf55c9a149ce5c22c4f",
+    "url": "/static/js/2.7b915eca.chunk.js"
+  },
+  {
+    "revision": "b74364752c6386920aced16a152e4b46",
+    "url": "/static/js/2.7b915eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8eefad631e36adce77",
+    "url": "/static/js/main.514cccd3.chunk.js"
+  },
+  {
+    "revision": "984cf404eae01ab73fc5",
+    "url": "/static/js/runtime-main.1c418fa7.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
