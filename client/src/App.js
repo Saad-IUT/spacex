a75ef12eb834efc969fac291d0630d5a17150dc6
Saad <saadbinjohir@gmail.com>
2020-07-10 19:56:55 +0600

    Finishing The App

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Launches from './components/Launches'
+import Launch from './components/Launch'
 import './App.css'
-import Launches from './components/Launches';
 import logo from './logo.png'
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
 })
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className='container'>
-        <img
-          src={logo}
-          alt='SpaceX'
-          style={{ width: 300, display: 'block', margin: 'auto' }}
-        />{' '}
-        <Launches/>
-      </div>
-    </ApolloProvider>
-  )
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className='container'>
+            <img
+              src={logo}
+              alt='SpaceX'
+              style={{ width: 300, display: 'block', margin: 'auto' }}
+            />
+            <Route exact path='/' component={Launches} />
+            <Route exact path='/launch/:flight_number' component={Launch} />
+          </div>
+        </Router>
+      </ApolloProvider>
+    )
+  }
 }
 
 export default App
